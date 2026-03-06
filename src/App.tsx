e7a7657f604ba8b0637a620747275543e41a972c
YoungSik Oh <dudtlr9308@naver.com>
2026-03-06 13:45:39 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, { JSX } from 'react';
+import './App.css';
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>테스트 확인</h1>
+        <p>
+          여기까지 성공
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
